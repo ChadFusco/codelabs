b09fd29cd78c04ec823f532a20483ddac7e86c8e
ChadFusco <fusco@case.edu>
2023-08-17 15:09:45 -0700

    completed routing to details component pages

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -7,11 +8,16 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <p>
-      details works!
+      details works! {{ housingLocationId }}
     </p>
   `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
+  // ActivatedRoute is a reference to the current route we're matched to in the app (thanks to the routes in routes.ts)
+  housingLocationId: number;
 
+  constructor(private route: ActivatedRoute) {
+    this.housingLocationId = +this.route.snapshot.params["id"];
+  }
 }
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,18 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housing-location';
-
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ CommonModule, RouterModule ], // these are DEPENDENCIES
   template: `
 <section class="listing">
   <!-- Put attribute in square brackets below for property binding! -->
   <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
   <h2 class="listing-heading">{{ housingLocation.name }}</h2>
   <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+  <!-- Code below links to new url. For example: /details/2 -->
+  <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+</section>
   `,
   styleUrls: ['./housing-location.component.css']
 })
