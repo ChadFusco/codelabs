e781aac79a9ccb81ced18f05fe4e4814cc0b9ac1
ChadFusco <fusco@case.edu>
2023-08-16 21:57:20 -0700

    implemented skeleton for HousingLocationComponent

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -6,9 +6,10 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+<section class="listing">
+  <img class="listing-photo">
+  <h2 class="listing-heading"></h2>
+  <p class="listing-location"></p>
   `,
   styleUrls: ['./housing-location.component.css']
 })
